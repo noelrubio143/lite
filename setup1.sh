178d764a2bb8996cfe2aba83f5b5ad85014e3377
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-30 11:43:27 +0800

    Refactor setup1.sh to streamline installation process
    Removed redundant installation commands and added additional service installations.

--- setup1.sh
+++ setup1.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cd
-# install stunnel
+# install service
+bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/xx)
+curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/slip/slipstream-rust-deploy.sh -o /tmp/slipstream-rust-deploy.sh && sudo mv /tmp/slipstream-rust-deploy.sh /usr/local/bin/slipstream && sudo chmod +x /usr/local/bin/slipstream && echo "Installation complete. Type 'slipstream' to open the SSH + slipstream menu."
+# Additional commands
+bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/swap.sh)
+curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/usernew.sh -o /tmp/usernew.sh && sudo mv /tmp/usernew.sh /usr/local/bin/user && sudo chmod +x /usr/local/bin/user && echo "Installation complete. Type 'user' to open the SSH + user menu."
+curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/hapus.sh -o /tmp/hapus.sh && sudo mv /tmp/hapus.sh /usr/local/bin/delete && sudo chmod +x /usr/local/bin/delete && echo "Installation complete. Type 'delete' to open the SSH + delete menu."
+curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/hapus.sh -o /tmp/member.sh && sudo mv /tmp/member.sh /usr/local/bin/list && sudo chmod +x /usr/local/bin/list && echo "Installation complete. Type 'list' to open the SSH + list menu."
+# intall stunnel
 apt install stunnel4 -y
 cat > /etc/stunnel/stunnel.conf <<-END
 cert = /etc/stunnel/stunnel.pem
@@ -12,18 +19,3 @@ socket = r:TCP_NODELAY=1
 [dropbear]
 accept = 443
 connect = 127.0.0.1:22
-
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/xx)
-curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/slip/slipstream-rust-deploy.sh -o /tmp/slipstream-rust-deploy.sh && sudo mv /tmp/slipstream-rust-deploy.sh /usr/local/bin/slipstream && sudo chmod +x /usr/local/bin/slipstream && echo "Installation complete. Type 'slipstream' to open the SSH + slipstream menu."
-# Additional commands
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/swap.sh)
-curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/usernew.sh -o /tmp/usernew.sh && sudo mv /tmp/usernew.sh /usr/local/bin/user && sudo chmod +x /usr/local/bin/user && echo "Installation complete. Type 'user' to open the SSH + user menu."
-curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/hapus.sh -o /tmp/hapus.sh && sudo mv /tmp/hapus.sh /usr/local/bin/delete && sudo chmod +x /usr/local/bin/delete && echo "Installation complete. Type 'delete' to open the SSH + delete menu."
-curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/hapus.sh -o /tmp/member.sh && sudo mv /tmp/member.sh /usr/local/bin/list && sudo chmod +x /usr/local/bin/list && echo "Installation complete. Type 'list' to open the SSH + list menu."
-# Cleanup and reboot
-rm -f /root/setup.sh /root/ins-xray.sh /root/insshws.sh cf ssh-vpn.sh ins-xray.sh insshws.sh
-echo "Auto reboot in 10 seconds..."
-sleep 10
-
-# Reboot
-reboot
