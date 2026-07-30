66ac4e17ed855e57943ff775164c432cd9b5d576
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-30 10:23:17 +0800

    Clean up setup1.sh by removing unused setup commands
    Removed environment setup for auto-reboot and log setup.

--- setup1.sh
+++ setup1.sh
@@ -4,15 +4,6 @@ wget -q https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/slip
 dos2unix slipstream-rust-deploy.sh
 bash slipstream-rust-deploy.sh
 
-# Setup environment for auto-reboot
-ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
-
-# Log setup
-mkdir -p /var/lib/
-echo "IP=" >> /var/lib/ipvps.conf
-
 # Additional commands
 bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/swap.sh)
 bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/member.sh)
