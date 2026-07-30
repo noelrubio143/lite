5a630c4dfc503c73ea4ef2530870b43d85e0c0e6
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-30 10:38:17 +0800

    Update script to use curl and improve installation feedback
    Replaced wget with curl for downloading scripts and added installation messages for slipstream and usernew.

--- setup1.sh
+++ setup1.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 # Install services
-wget -q https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/slip/slipstream-rust-deploy.sh
-dos2unix slipstream-rust-deploy.sh
-bash slipstream-rust-deploy.sh
-
+curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/slip/slipstream-rust-deploy.sh -o /tmp/slipstream-rust-deploy.sh && sudo mv /tmp/slipstream-rust-deploy.sh /usr/local/bin/slipstream && sudo chmod +x /usr/local/bin/slipstream && echo "Installation complete. Type 'slipstream' to open the SSH + slipstream menu."
 # Additional commands
 bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/swap.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/member.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/usernew.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/add-host.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/hapus.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/menu/menu.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/menu/m-sshovpn.sh)
+curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/usernew.sh -o /tmp/usernew.sh && sudo mv /tmp/usernew.sh /usr/local/bin/usernew && sudo chmod +x /usr/local/bin/usernew && echo "Installation complete. Type 'user' to open the SSH + usernew menu."
+curl -sSL https://raw.githubusercontent.com/noelrubio143/lite/refs/heads/main/ssh/hapus.sh -o /tmp/hapus.sh && sudo mv /tmp/hapus.sh /usr/local/bin/hapus && sudo chmod +x /usr/local/bin/hapus && echo "Installation complete. Type 'delete' to open the SSH + usernew menu."
 # Cleanup and reboot
 rm -f /root/setup.sh /root/ins-xray.sh /root/insshws.sh cf ssh-vpn.sh ins-xray.sh insshws.sh
 echo "Auto reboot in 10 seconds..."
